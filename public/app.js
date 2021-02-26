787ca7878244de33b5b3f0bf582097d68c00fd29
Chase Lancaster <chaseblancaster@gmail.com>
2021-02-25 20:38:03 -0800

    add form as variable with queryselector grabbing the form and all add the inputs type casted as HTML elemetns

--- public/app.js
+++ public/app.js
@@ -1,1 +1,12 @@
 "use strict";
+var form = document.querySelector(".new-item-form");
+console.log(form.children);
+// inputs
+var type = document.querySelector("#type");
+var toFrom = document.querySelector("#toFrom");
+var details = document.querySelector("#details");
+var amount = document.querySelector("#amount");
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log(type.value, toFrom.value, details.value, amount.value);
+});
