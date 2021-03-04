c4c8a277cd5b01e63a6e2304681d8877b481fabb
Chase Lancaster <chaseblancaster@gmail.com>
2021-03-03 17:19:24 -0800

    add abasic interface to practice structure

--- public/app.js
+++ public/app.js
@@ -1,4 +1,15 @@
 import { Invoice } from "./classes/Invoice.js";
+const me = {
+    name: "Chase",
+    age: 25,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log("I spent, amount");
+        return amount;
+    },
+};
 const invOne = new Invoice("mario", "work on the mario website", 250);
 const invTwo = new Invoice("luigi", "work on the luigi website", 300);
 let invoices = [];
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,25 @@
 import { Invoice } from "./classes/Invoice.js";
 
+// interfaces
+interface IsPerson {
+  name: string;
+  age: number;
+  speak(a: string): void;
+  spend(a: number): void;
+}
+
+const me: IsPerson = {
+  name: "Chase",
+  age: 25,
+  speak(text: string): void {
+    console.log(text);
+  },
+  spend(amount: number): number {
+    console.log("I spent, amount");
+    return amount;
+  },
+};
+
 const invOne = new Invoice("mario", "work on the mario website", 250);
 const invTwo = new Invoice("luigi", "work on the luigi website", 300);
 
