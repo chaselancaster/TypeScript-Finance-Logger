8ba14a50248b7c12c47c9c17d0439b37095de4bc
Chase Lancaster <chaseblancaster@gmail.com>
2021-03-03 17:53:12 -0800

    add ListTemplate with a render method that appends the form information into an li tag

--- public/app.js
+++ public/app.js
@@ -1,35 +1,23 @@
 import { Invoice } from "./classes/Invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
 import { Payment } from "./classes/Payment.js";
-let docOne;
-let docTwo;
-docOne = new Invoice("yoshi", "web work", 250);
-docTwo = new Payment("mario", "plumbing work", 200);
-let docs = [];
-docs.push(docOne);
-docs.push(docTwo);
-const me = {
-    name: "Chase",
-    age: 25,
-    speak(text) {
-        console.log(text);
-    },
-    spend(amount) {
-        console.log("I spent, amount");
-        return amount;
-    },
-};
-const invOne = new Invoice("mario", "work on the mario website", 250);
-const invTwo = new Invoice("luigi", "work on the luigi website", 300);
-let invoices = [];
-invoices.push(invOne);
-invoices.push(invTwo);
 const form = document.querySelector(".new-item-form");
 // inputs
 const type = document.querySelector("#type");
 const toFrom = document.querySelector("#toFrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
+// list template instance
+const ul = document.querySelector("ul");
+const list = new ListTemplate(ul);
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log(type.value, toFrom.value, details.value, amount.value);
+    let doc;
+    if (type.value === "invoice") {
+        doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
+    }
+    list.render(doc, type.value, "end");
 });
--- src/app.ts
+++ src/app.ts
@@ -1,43 +1,7 @@
 import { Invoice } from "./classes/Invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
 import { Payment } from "./classes/Payment.js";
 import { HasFormatter } from "./interfaces/HasFormatter.js";
-
-let docOne: HasFormatter;
-let docTwo: HasFormatter;
-
-docOne = new Invoice("yoshi", "web work", 250);
-docTwo = new Payment("mario", "plumbing work", 200);
-
-let docs: HasFormatter[] = [];
-docs.push(docOne);
-docs.push(docTwo);
-
-// interfaces
-interface IsPerson {
-  name: string;
-  age: number;
-  speak(a: string): void;
-  spend(a: number): void;
-}
-
-const me: IsPerson = {
-  name: "Chase",
-  age: 25,
-  speak(text: string): void {
-    console.log(text);
-  },
-  spend(amount: number): number {
-    console.log("I spent, amount");
-    return amount;
-  },
-};
-
-const invOne = new Invoice("mario", "work on the mario website", 250);
-const invTwo = new Invoice("luigi", "work on the luigi website", 300);
-
-let invoices: Invoice[] = [];
-invoices.push(invOne);
-invoices.push(invTwo);
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 
@@ -47,8 +11,19 @@ const toFrom = document.querySelector("#toFrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
 
+// list template instance
+const ul = document.querySelector("ul")!;
+const list = new ListTemplate(ul);
+
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
 
-  console.log(type.value, toFrom.value, details.value, amount.value);
+  let doc: HasFormatter;
+  if (type.value === "invoice") {
+    doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+  } else {
+    doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
+  }
+
+  list.render(doc, type.value, "end");
 });
